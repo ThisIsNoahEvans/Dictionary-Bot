ae3b775172557a38e6c40322c707ce732595d1c4
Noah Evans <noah@itsnoahevans.co.uk>
2019-09-24 17:34:45 +0100

    Update MainBot.py
    -fixed minor issues
    -added function to prevent words being used twice
    -added function to bypass explicit words

--- MainBot.py
+++ MainBot.py
@@ -1,100 +1,123 @@
 ###This is the main script for the bot to work###
 
-#Repeats For Every Word In words.txt
+#Imports Required Libraries
+from PyDictionary import PyDictionary
+import tweepy
+import random
+import sys
+import time
+
+#Sets Font For Print
+class colour:
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
+
+#Define Dictionary and Connect To Twitter API
+dictionary = PyDictionary()
+auth = tweepy.OAuthHandler("rrqJVjuQtL12ITAV2kBwa7tM1", "wMpNoC436MjXJnbR1GIKtS2fo89kViHkd8WerWQQ6XJyLM3HvA")
+auth.set_access_token("1036917176983277568-9OANYg29Kfn1eMg5XgTMx1oCu7y7x0", "ILS2IxajT2nfpChEQqdLZ9zbPTNkGEcFdmSzN4lcj1NEJ")
+api = tweepy.API(auth)
+
+#Print Program Status
+print(colour.BOLD, colour.PURPLE, "Imported Libraries And Connected To The Twitter API", colour.END)
+
+#Starts Tweet Loop
 for x in range(1, 370103):
-	#Imports Required Libraries
-	from PyDictionary import PyDictionary
-	import tweepy
-	import random
-	import time
-
-	#Sets Font For Print Statements
-	class colour:
-		PURPLE = '\033[95m'
-		CYAN = '\033[96m'
-		DARKCYAN = '\033[36m'
-		BLUE = '\033[94m'
-		GREEN = '\033[92m'
-		YELLOW = '\033[93m'
-		RED = '\033[91m'
-		BOLD = '\033[1m'
-		UNDERLINE = '\033[4m'
-		END = '\033[0m'
-
-	#Defines Dictionary And Connects To Twitter API
-	dictionary = PyDictionary()
-	auth = tweepy.OAuthHandler("INSERT YOUR API", "INSERT  YOUR API")
-	auth.set_access_token("INSERT YOUR API", "INSERT YOUR API")
-	api = tweepy.API(auth)
-
 	#Picks Random Word
 	with open('words.txt') as list:
-	        #Scans The File
-	        lines = list.readlines()
-	        #Sets randomStr As The Generated String
-	        randomStr = random.choice(lines)
-	        #Sets randomStr As A String And Keeps The Original Value
-	        randomStr = str(randomStr)
-	        #Removes Blank Line After The Word
-	        randomStr = randomStr.replace("\n", "")
+		#Scan The File
+		lines = list.readlines()
+		#Set randomStr As The Generated String
+		randomStr = random.choice(lines)
+		#Set randomStr As A String And Keeps The Original Value
+		randomStr = str(randomStr)
+		#Remove Blank Line After The Word
+		randomStr = randomStr.replace("\n", "")
+		#Save Word To Text File Of Used Words
+		usedWrite = open('UsedWords.txt', 'a')
+		usedWrite.write(randomStr + '\n')
+		print(colour.BOLD, colour.RED, 'Added Used Word To Used List')
 
-	#Prints Program Status
-	print(colour.BOLD, "Imported Libraries And Connected To The Twitter API", colour.END)
-	print(colour.BOLD, "Selected Random Word", colour.END)
+	#Checks For Already Used Word
+	with open ('UsedWords.txt') as usedRead:
+		if randomStr in usedRead:
+			print(colour.BOLD, colour.BLUE, 'Word already Tweeted, restarting...')
+			print(randomStr)
+			#Returns To Start Of Loop
+			continue
 
-	#Sets Tweet
+	#Checks For Blocked Word
+	with open('BlockedWords.txt') as blocked:
+		if randomStr in blocked:
+			print(colour.BOLD, colour.BLUE, 'Blocked word, restarting...', colour.END)
+			print(tweet)
+			#Returns To Start Of Loop
+			continue
+
+	#Print Program Status
+	print(colour.BOLD, colour.RED, "Selected Random Word", colour.END)
+
+	#Set Tweet
 	original_tweet_text = dictionary.meaning(randomStr)
-	#Prints Program Status
-	print(colour.BOLD, "Got Definition Of The Word", colour.END )
-	#Sets original_tweet_text As A String And Keeps The Original Value
+	#Print Program Status
+	print(colour.BOLD, colour.RED, "Got Definition Of The Word", colour.END )
+	#Set original_tweet_text As A String And Keeps The Original Value
 	original_tweet_text = str(original_tweet_text)
-	#Removes Certain Characters
-	original_tweet_text = original_tweet_text.replace("]}", "")
+	#Remove Certain Characters
+	original_tweet_text = original_tweet_text.replace("]", "")
 	original_tweet_text = original_tweet_text.replace("{", "")
 	original_tweet_text = original_tweet_text.replace("[", "")
+	original_tweet_text = original_tweet_text.replace("]", "")
+	original_tweet_text = original_tweet_text.replace("}", "")
 	original_tweet_text = original_tweet_text.replace("'", "")
 	original_tweet_text = original_tweet_text.replace("(", "")
 
-	#Capitalises First Letter Of Word And Add Some Words
-	#Switch Between 'A' And 'An'
-	##Adverb
+	#Capitalise First Letter Of Word And Add Some Words:
+	#Switch Between 'A' And 'An':
+	#Adverb
 	if "Adverb" in original_tweet_text:
 	    tweet = randomStr.capitalize() + " is an " + original_tweet_text
 	else:
-	    ##Noun
+	    #Noun
 	    if "Noun" in original_tweet_text:
 	        tweet = randomStr.capitalize() + " is a " + original_tweet_text
 	    else:
-	        #A#djective
+	        #Adjective
 	        if "Adjective" in original_tweet_text:
 	            tweet = randomStr.capitalize() + " is an " + original_tweet_text
 	        else:
-	            ##Verb
+	            #Verb
 	            if "Verb" in original_tweet_text:
 	                tweet = randomStr.capitalize() + " is a " + original_tweet_text
 	            else:
-	                ##Infinitive
+	                #Infinitive
 	                if "Infinitive" in original_tweet_text:
 	                    tweet = randomStr.capitalize() + " is an " + original_tweet_text
 	                else:
-	                    ##Everything Else
+	                    #Everything Else
 	                    tweet = randomStr.capitalize() + " is a " + original_tweet_text
 
-	#Restarts If No Definition
+	#Exits If No Definition
 	if "None" in tweet:
-		print(colour.BOLD, 'No definition; exitting...', colour.END)
+		print(colour.BOLD, colour.BLUE, 'No definition, restarting...', colour.END)
 		print(tweet)
-		#Returns To Start Of Loop
 		continue
 
-	#Adds Full Stop
+	#Add Full Stop
 	tweet = tweet + '.'
-	#Truncates Tweet And Adds'[..]' If Tweet Exceeds Twitter Limit
+	#Truncate Tweet And Add '[..]' If It Is Too Long
 	tweet = tweet[:276] + (tweet[276:] and '[..]')
-	#Tweets
+	#Tweet
 	api.update_status(tweet)
-	#Prints Output
-	print(colour.BOLD, "\"" + tweet + "\" Was Tweeted!", colour.END)
-	print(colour.BOLD, 'WAITING 15 MINUTES (900 SECONDS) TO TWEET AGAIN', colour.END)
-	#Pauses for 15 minutes
-	time.sleep(900)
+	#Print Output
+	print(colour.BOLD, colour.PURPLE, "\"" + tweet + "\" Was Tweeted!", colour.END)
+	print(colour.BOLD, colour.BLUE, 'WAITING 15 MINUTES (900 SECONDS) TO TWEET AGAIN', colour.END)
+	time.sleep(870)
